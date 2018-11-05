413425b6239c05dbd425f28e20086edc107dea92
Adasiek laptop <adasiek-laptop@abix.info.pl>
2018-11-05 09:37:18 +0100

    test

--- PyTechBrain/PyTechBrain.py
+++ PyTechBrain/PyTechBrain.py
@@ -2,6 +2,7 @@ from pyfirmata import *
 
 class PyTechBrain(object):
 
+
     def __init__(self):
         self = util.get_the_board(base_dir='/dev/serial/by-id/', identifier='usb-')
         self.PWM = self.get_pin('d:9:p')
